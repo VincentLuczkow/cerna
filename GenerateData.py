f40d8569056250b8ae38fd1fa66a383104ee70b9
vincent <luczkow.vincent@gmail.com>
2016-04-19 18:37:25 -0400

    Added README

--- GenerateData.py
+++ GenerateData.py
@@ -18,23 +18,21 @@ def main():
         # Set up network
         ks, mus, gammas = Network.Network.generate_parameters(x, y)
         current_network = Network.Network(x, y, ks, mus, gammas)
-        current_network.add_test("gamma")
-        current_network.add_decay_estimator("gamma", test, "ode")
-        #set_trace()
+        current_network.add_test("wild")
         current_network.add_test("lambda")
-        current_network.add_decay_estimator("lambda", test, "ode")
-        gamma_relative_error += current_network.estimates[("gamma", "ode", test)].average_relative_error
-        gamma_rmse += current_network.estimates[("gamma", "ode", test)].average_rmse
-        lambda_relative_error += current_network.estimates[("lambda", "ode", test)].average_rmse
-        lambda_rmse += current_network.estimates[("lambda", "ode", test)].average_rmse
+        current_network.add_test("gamma")
+        current_network.add_test("knockout")
+        current_network.add_simple_decay_estimator("gamma", "ode", test)
+        current_network.add_simple_decay_estimator("lambda", "ode", test)
+
     gamma_relative_error /= number_of_tests
     gamma_rmse /= number_of_tests
-    lambda_relative_error /= number_of_tests
-    lambda_rmse /= number_of_tests
+    #lambda_relative_error /= number_of_tests
+    #lambda_rmse /= number_of_tests
     print("Gamma PE: {0}".format(gamma_relative_error))
     print("Gamma RMSE: {0}".format(gamma_rmse))
-    print("Lambda PE: {0}".format(lambda_relative_error))
-    print("Lambda RMSE: {0}".format(lambda_rmse))
+    #print("Lambda PE: {0}".format(lambda_relative_error))
+    #print("Lambda RMSE: {0}".format(lambda_rmse))
 
 
 if __name__ == "__main__":
---
+++ README.md
@@ -0,0 +1,7 @@
+# README
+
+## Code Overview
+
+This code generates, simulates, and analyzes ceRNA networks. It can generate many different types of ceRNA networks, and
+can easily be extended to generate more. It can simulate the networks exactly using the Gillespie algorithm (implemented
+by the StochPy library), and using a numerical solution to a mean field approximation.
